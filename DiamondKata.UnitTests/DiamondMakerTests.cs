4a1d561e8008f131c89a040a4a275c6357f68f7a
piotrlukasik <lukcode@outlook.com>
2024-05-07 01:30:20 +0200

    Extends positive and negative testing scenarions

--- DiamondKata.UnitTests/DiamondMakerTests.cs
+++ DiamondKata.UnitTests/DiamondMakerTests.cs
@@ -19,4 +19,67 @@ public class DiamondMakerTests
         // Assert
         Assert.That(actualDiamond, Is.EqualTo(expectedDiamond));
     }
+
+    [Test]
+    public void CreateDiamond_ValidInput_InputC_ReturnsDiamondPattern()
+    {
+        // Arrange
+        var diamondMaker = new DiamondMaker();
+        string[] input = ["C"];
+        const string expectedDiamond = @"  A
+ B B
+C   C
+ B B
+  A";
+
+        // Act
+        var actualDiamond = diamondMaker.CreateDiamond(input);
+
+        // Assert
+        Assert.That(actualDiamond, Is.EqualTo(expectedDiamond));
+    }
+
+    [Test]
+    public void CreateDiamond_ValidInput_InputD_ReturnsDiamondPattern()
+    {
+        // Arrange
+        var diamondMaker = new DiamondMaker();
+        string[] input = ["D"];
+        const string expectedDiamond = @"   A
+  B B
+ C   C
+D     D
+ C   C
+  B B
+   A";
+
+        // Act
+        var actualDiamond = diamondMaker.CreateDiamond(input);
+
+        // Assert
+        Assert.That(actualDiamond, Is.EqualTo(expectedDiamond));
+    }
+
+    [TestCaseSource(nameof(InvalidInputTestCases))]
+    public void CreateDiamond_InvalidInput_ThrowsArgumentException(string[] input)
+    {
+        // Arrange
+        var expectedErrorMessage = "Invalid input. Please provide one uppercase character from A to Z.";
+        var diamondMaker = new DiamondMaker();
+
+        // Act & Assert
+        var exception = Assert.Throws<ArgumentException>(() => diamondMaker.CreateDiamond(input));
+        Assert.That(exception?.Message, Is.EqualTo(expectedErrorMessage));
+    }
+
+    private static IEnumerable<string[]> InvalidInputTestCases()
+    {
+        yield return ["a"]; // Lowercase letter
+        yield return ["0"]; // Digit
+        yield return ["_"]; // Special character
+        yield return [" "]; // Empty string
+        yield return ["AB"]; // More than one character
+        yield return ["z"]; // Valid character (but still throws because it's lowercase)
+        yield return ["A","D"]; // More than 1 argument
+    }
 }
